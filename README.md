64ea301db38870d12d573983cf5e03aa83ebba64
li <3503823799@qq.com>
2025-05-21 20:44:07 +0800

    添加了聊天记忆功能和流式输出

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 来到项目所在文件夹，在命令行窗口输入`streamlit run steamlit_demo.py`
+
+<div align="center">程序界面</div>
+
+![img.png](img.png)
--- streamlit_demo.py
+++ streamlit_demo.py
@@ -7,29 +7,36 @@ client = OpenAI(
     base_url="https://dashscope.aliyuncs.com/compatible-mode/v1",
 )
 
-st.title('小白只能助手')
+st.title('小白ai助手')
 
 st.divider()
-st.write("你好")
+# st.write("你好")
 
 prompt = st.chat_input("给小白发送消息")
 
 if "messages" not in st.session_state:
-    st.session_state["messages"] = []
+    st.session_state["messages"] = [{"role": "system", "content": "你是一个乐于助人的人工智能助手，你的名字叫小白"}]
 
 if prompt:
     st.session_state["messages"].append({"role": "user", "content": prompt})
     for message in st.session_state["messages"]:
+        if message["role"] == "system":
+            continue
         st.chat_message(message["role"]).markdown(message["content"])
 
     with st.spinner("思考中..."):
         completion = client.chat.completions.create(
             model="qwen-plus",
-            messages=[
-                {"role": "system", "content": "你是一个乐于助人的人工智能助手，你的名字叫小白"},
-                {"role": "user", "content": prompt},
-            ],
+            messages=st.session_state["messages"],
+            stream=True,
+            stream_options={"include_usage": True}
         )
-        response = completion.choices[0].message.content
+        response = ""
+        chat = st.chat_message("assistant").markdown(response)
+        for chunk in completion:
+            if not chunk.choices:
+                continue
+            choice = chunk.choices[0]
+            response += choice.delta.content
+            chat.markdown(response)
         st.session_state["messages"].append({"role": "assistant", "content": response})
-        st.chat_message("assistant").markdown(response)
